8ec1086872c33fe13cba0c532312f386279a518e
Albin Sunnanbo <albin@sunnanbo.com>
2014-11-06 19:12:10 +0100

    Refactor: move script to chat-script to separate file
    Remove unused about and contact pages

---
+++ SignalR-playground/SignalR-playground/Scripts/home.index.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    // Reference the auto-generated proxy for the hub.
+    var chat = $.connection.signalRHub;
+    // Create a function that the hub can call back to display messages.
+    chat.client.hello = function (message) {
+        // Add the message to the page.
+        $('#output').append('<p>' + htmlEncode(message) + '</p>');
+    };
+    // Get the user name and store it to prepend to messages.
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        // Call the Send method on the hub.
+        chat.server.hello("connect");
+
+        $("#hello-button").click(function (event) {
+            chat.server.hello($('#user-name').val());
+        });
+    });
+});
+// This optional function html-encodes messages for display in the page.
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
